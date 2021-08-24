77210a4b05f627a85666e428ec772a7c43356f2e
Pratik Pathak <pratikpathak200@gmail.com>
2021-08-24 11:23:26 +0530

    web search completed

--- tracker.py
+++ tracker.py
@@ -14,12 +14,17 @@ browser = webdriver.Chrome(options=options)
 
 def main():
     pincode = list(map(int, os.getenv("PINCODES").split(" ")))
-    semail = os.getenv("Email")
-
+    email = os.getenv("Email")
+    
     browser.get("https://www.cowin.gov.in/")
+    
+    browser.find_element_by_id('mat-input-0').send_keys("400009")
+    browser.find_element_by_id("mat-tab-content-0-0").click()
+    browser.find_element_by_xpath("//*[@id=\"Search-Vaccination-Center\"]/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[2]/div[3]/ul/li[2]/div/div[2]/label").click()
+    
 
     # for i in pincode:
-    #     browser.find_element_by_id(“mat-input-0”).send_keys(i)
+    #     browser.find_element_by_id('mat-input-0').send_keys(i)
 
 
 if __name__ == "__main__":
