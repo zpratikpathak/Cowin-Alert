8c5a931f2633610e54deab028d4228d70262e18c
Pratik Pathak <pratikpathak200@gmail.com>
2021-08-24 12:01:24 +0530

    No shot found feature added

--- tracker.py
+++ tracker.py
@@ -22,13 +22,17 @@ def main():
     browser.find_element_by_id('mat-input-0').send_keys("400009")
     browser.implicitly_wait(10)
     time.sleep(2)
-    browser.find_element_by_xpath("//*[@id=\"mat-tab-content-0-0\"]/div/div[1]/div/div/button").click()
-    # browser.find_element_by_css_selector('.searchBtn .pin-search-btn .accessibility-plugin-ac').click()
-    # browser.find_element_by_xpath("/html/body/app-root/div/app-home/div[3]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/form/mat-tab-group/div/mat-tab-body[1]/div/div[1]/div/div/button").click()
-    
+    browser.find_element_by_xpath("//*[@id=\"mat-tab-content-0-0\"]/div/div[1]/div/div/button").click()   
     time.sleep(2)
     browser.find_element_by_xpath("//*[@id=\"Search-Vaccination-Center\"]/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[2]/div[3]/ul/li[2]/div/div[2]/label").click()
-    
+    time.sleep(2)
+    try:
+        browser.find_element_by_xpath("//*[@id=\"Search-Vaccination-Center\"]/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[5]/div[3]/div/div/div[1]/p")
+        #No slots
+        print("No shots")
+    except:
+        #sendemail()
+        pass
 
     # for i in pincode:
     #     browser.find_element_by_id('mat-input-0').send_keys(i)
