2cfdc07f18e16ce8e07b8bf4a8d85d7aaf7cb891
Pratik Pathak <pratikpathak200@gmail.com>
2021-08-24 11:09:17 +0530

    Cowin webpage Fetched

--- tracker.py
+++ tracker.py
@@ -10,12 +10,16 @@ options.add_argument(
     "user-agent='User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'"
 )
 browser = webdriver.Chrome(options=options)
-browser.get("google.com")
 
 
 def main():
     pincode = list(map(int, os.getenv("PINCODES").split(" ")))
     semail = os.getenv("Email")
+
+    browser.get("https://www.cowin.gov.in/")
+
+    # for i in pincode:
+    #     browser.find_element_by_id(“mat-input-0”).send_keys(i)
 
 
 if __name__ == "__main__":
